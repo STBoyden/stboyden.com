ed4a1197ff79937e1a5ad89b29bb4d5e6cfd98e8
STBoyden <sam@stboyden.com>
2021-03-05 19:10:38 +0000

    Modify CSS of NotFound.svelte

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -2,10 +2,10 @@
 <p>This route doesn't exist.</p>
 
 <style>
-	h1 {
-		color: #ff0040;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff0040;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 </style>
