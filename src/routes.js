bda2c9f14869efa0814ceee9d1d39f7f90f40487
STBoyden <sam@stboyden.com>
2021-03-05 19:10:21 +0000

    Add Contact page component

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import Home from './routes/Home.svelte';
 import NotFound from './routes/NotFound.svelte';
+import Contact from './routes/Contact.svelte';
 
 export default {
     '/': Home,
+    '/contact_me': Contact,
     // The catch-all route must always be last
     '*': NotFound
 };
---
+++ src/routes/Contact.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    let details = {
+        Email: "<a href='mailto:sam@stboyden.com'>sam@stboyden.com</a>",
+        Phone: "+44 7581 750711",
+    };
+    let keys = [...Object.keys(details)];
+</script>
+
+<div in:fade={{ delay: 100 }}>
+    <h3>You can contact me through these methods</h3>
+    {#each keys as k}
+        <h2
+            in:fade={{
+                delay: 500 * keys.indexOf(k),
+            }}
+        >
+            {k}: {@html details[k]}
+        </h2>
+    {/each}
+</div>
