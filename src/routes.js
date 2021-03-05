9d4236dff481a5e94dd2b3f5848949e5edf81a53
STBoyden <sam@stboyden.com>
2021-03-05 22:42:17 +0000

    Added Projects page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Home from './routes/Home.svelte';
 import NotFound from './routes/NotFound.svelte';
 import Contact from './routes/Contact.svelte';
+import Projects from './routes/Projects.svelte';
 
 export default {
     '/': Home,
     '/contact_me': Contact,
+    '/projects': Projects,
     // The catch-all route must always be last
     '*': NotFound
 };
---
+++ src/routes/Projects.svelte
@@ -0,0 +1,50 @@
+<script>
+    import ProjectCard from "../components/ProjectCard.svelte";
+
+    let projects = [
+        {
+            project_name: "Ocean",
+            project_link: "https://github.com/STBoyden/ocean",
+            description:
+                "A C/C++ build system/project manager written in Rust.",
+            picture_url:
+                "https://raw.githubusercontent.com/STBoyden/ocean/master/logo.svg",
+        },
+        {
+            project_name: "stboyden.com",
+            project_link: "https://github.com/STBoyden/stboyden.com",
+            description: "Personal website written in Svelte + JS.",
+        },
+        {
+            project_name: "market-api",
+            project_link: "https://github.com/MinesoftCC/market-api",
+            description:
+                "Market API for an Minecraft-ComputerCraft based economy. Intended to be used in conjunction with <a href='https://github.com/MinesoftCC/CCash'>this</a>. Written in Rust.",
+        },
+        {
+            project_name: "market-dashboard",
+            project_link: "https://github.com/MinesoftCC/market-dashboard",
+            description:
+                "A front-end for the market API created by me. Designed to work alongside the bank API developed by EntireTwix. Written in Rust.",
+        },
+    ];
+</script>
+
+<div id="projects">
+    <h1>Projects</h1>
+    {#each projects as project}
+        <ProjectCard {...project} delay={750 * projects.indexOf(project) + 1} />
+    {/each}
+</div>
+
+<style>
+    #projects {
+        margin-top: 5vh;
+    }
+
+    @media (max-width: 640px) {
+        #projects {
+            padding: 10px;
+        }
+    }
+</style>
