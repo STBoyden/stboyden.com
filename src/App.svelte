85a2fc35053851785239bd2878187a710ad4a902
STBoyden <sam@stboyden.com>
2021-03-05 19:10:06 +0000

    Add Navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
     import Router from "svelte-spa-router";
+    import Navbar from "./components/Navbar.svelte";
     import routes from "./routes";
 </script>
 
 <main>
+    <Navbar />
     <Router {routes} />
 </main>
 
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,41 @@
+<ul id="navbar">
+    <li class="navbutton"><a href="#/">Home</a></li>
+    <li class="navbutton">
+        <a href="#/projects">Projects</a>
+    </li>
+    <li class="navbutton">
+        <a href="#/contact_me">Contact Me</a>
+    </li>
+</ul>
+
+<style>
+    #navbar {
+        display: inline-block;
+        width: 100vw;
+        padding: 0;
+        margin: 0;
+        background-color: #333;
+    }
+
+    .navbutton {
+        display: inline-block;
+        list-style-type: none;
+        padding: 1em;
+        background-color: #333;
+        margin: 0;
+    }
+
+    .navbutton:hover {
+        background-color: #555;
+    }
+
+    .navbutton a {
+        color: white;
+    }
+
+    @media (max-width: 640px) {
+        #navbar {
+            display: none;
+        }
+    }
+</style>
