960f2d54520f7ab3c93659d42d1f7be55ea81757
STBoyden <sam@stboyden.com>
2021-10-13 15:26:48 +0100

    Add darkmode toggle

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
     import Router from "svelte-spa-router";
     import Navbar from "./components/Navbar.svelte";
+    import DarkmodeButton from './components/DarkmodeButton.svelte'
     import routes from "./routes";
+
+    let body = document.getElementsByTagName("body")[0];
+
+    body.className = localStorage.getItem("display_mode") || "light";
 </script>
 
 <Navbar />
+<DarkmodeButton />
 <main>
     <Router {routes} />
 </main>
---
+++ src/components/DarkmodeButton.svelte
@@ -0,0 +1,42 @@
+<script>
+    let body = document.getElementsByTagName("body")[0];
+    let image_path = body.className == "dark" ? "dark" : "light";
+
+    const toggle_darkmode = () => {
+        let body = document.getElementsByTagName("body")[0];
+
+        if (body.className == "dark") {
+            body.className = "";
+            image_path = "light";
+            localStorage.setItem("display_mode", "light");
+        } else {
+            body.className = "dark";
+            image_path = "dark";
+            localStorage.setItem("display_mode", "dark");
+        }
+    }
+</script>
+
+<button id="darkmode-toggle" on:click={toggle_darkmode}>
+    <img class="darkmode-toggle-image" src={`${image_path}.svg`} alt />
+</button>
+
+<style>
+    #darkmode-toggle {
+        position: fixed;
+        top: 7.25px;
+        left: 0;
+        border: 0;
+        background-color: unset;
+    }
+
+    #darkmode-toggle:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+
+    .darkmode-toggle-image {
+        display: block;
+        width: 25px;
+        height: 25px;
+    }
+</style>
